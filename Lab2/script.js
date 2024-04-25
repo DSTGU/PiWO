eb1abf1ecb355280b68fd83ed89b57e7163e5dfc
DSTGU <zlypapiez2@gmail.com>
2024-04-26 00:33:35 +0200

    Kocham PIWO
    4.0 - Dodałem wyszukiwanie dynamiczne

--- Lab2/script.js
+++ Lab2/script.js
@@ -4,20 +4,19 @@ const createDiv = (inputText) => {
     //const listItem = document.createElement("li");
     const dziw = document.createElement("div")
     //listItem.innerText = input.value;
-    dziw.innerHTML = '<li class = "list-group-item"><p>' + inputText +'</p><p></p><button type="button" class="btn btn-danger position-absolute end-0 scale-50" onclick="removeme(this)">X</button></li>'
+    dziw.innerHTML = '<li class = "list-group-item"><p>' + inputText +'</p><p></p><button type="button" class="btn btn-danger position-absolute end-0 scale-50 translate-middle-y" onclick="removeme(this)">X</button></li>'
     // Assign an onclick event handler directly
+    dziw.id = (Math.random() + 1).toString(36).substring(2);
+
     dziw.addEventListener("click", function() {
-        console.log("Clicked")
         this.firstChild.firstChild.classList.toggle("disabled");
         this.firstChild.firstChild.classList.toggle("text-decoration-line-through");
         const date = new Date()
         
         if (this.firstChild.firstChild.classList.contains("disabled")) {
-            console.log("disabled");
             this.firstChild.firstChild.nextSibling.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
         }
         else {
-            console.log("enabled");
             this.firstChild.firstChild.nextSibling.innerHTML = "";
         }
     });
@@ -51,16 +50,45 @@ const removeme = (self) => {
     const modal = document.getElementById("potwierdzenie");
     const modalzadanie = document.getElementById("modalzadanie");
     modalzadanie.innerText = self.parentNode.parentNode.innerText;
+    const modid = document.getElementById("elementToRemID")
+
+    modid.innerHTML = self.parentNode.parentNode.id;
+
     modal.showModal();
+    
+    event.stopPropagation();
     //self.parentNode.parentNode.remove();
 }
 
+const searchbox = () => {
+    const value = document.getElementById("searchboxInput").value;
+    
+    const table = document.getElementById("todo-list");
+
+    var child=table.firstChild.nextSibling;
+
+    while (child !== null){
+
+        if(child.firstChild.firstChild.innerHTML.includes(value)){
+            child.firstChild.classList.remove("d-none");
+        }
+        else{
+            child.firstChild.classList.add("d-none");
+        }
+
+        child = child.nextSibling;
+    }
+
+}
 
 window.onload = () => {
 
     const closingButton = document.getElementById("closing");
     closingButton.addEventListener("click", () =>{
         
+        const val = document.getElementById("elementToRemID").innerHTML;
+        const elemtorem = document.getElementById(val);
+        elemtorem.remove();
         const modal = document.getElementById("potwierdzenie");
         modal.close();
 
